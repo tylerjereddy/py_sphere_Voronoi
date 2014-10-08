a5b8e6568081c3b45c4f6b231d015c7bfc7f5fad
Tyler Reddy <tyler.je.reddy@gmail.com>
2014-10-08 16:04:41 +0100

    -working on voronoi_utility.py: 	-removed the points argument from the Voronoi_Sphere_Surface class -- it is only retained in the __init__ method, where the calling arguments are passed 	-added a useful reference to the docstring of Voronoi_Sphere_Surface class 	-Delaunay_triangulation_spherical_surface() now returns the array of points for the vertices of each triangular facet of the convex hull, which based on visual inspection in an IPython notebook produces a very believable Delaunay triangulation of a test set of points on a sphere [but will eventually want proper unit testing, docstring example(s), etc.] 	-Voronoi_Sphere_Surface docstring now specifies the appropriate shape of the input points 	-minor typo fix(es) in docstring(s)

--- voronoi_utility.py
+++ voronoi_utility.py
@@ -6,15 +6,18 @@ import scipy
 import scipy.spatial
 import numpy
 
-class Voronoi_Sphere_Surface(points):
+class Voronoi_Sphere_Surface:
     '''Voronoi diagrams on the surface of a sphere.
     
     Parameters
     ----------
-    points: array
+    points: array, shape (npoints, 3)
         Coordinates of points to construct a Voronoi diagram on the surface of a sphere
     
+    References
+    ----------
     
+    .. [Caroli] Caroli et al. (2009) INRIA 7004
     
     '''
 
@@ -22,9 +25,15 @@ class Voronoi_Sphere_Surface(points):
         self.original_point_array = points
 
     def Delaunay_triangulation_spherical_surface(self):
-        '''Delaunay tesselation of the points on the surface of the sphere. This is simply the 3D convex hull of the points.'''
+        '''Delaunay tessellation of the points on the surface of the sphere. This is simply the 3D convex hull of the points. Returns a shape (N,3,3) array of points representing the vertices of the Delaunay triangulation on the sphere (i.e., N three-dimensional triangle vertex arrays).'''
         hull_instance = scipy.spatial.ConvexHull(self.original_point_array)
-        array_points_vertices_Convex_Hull = self.original_point_array[hull_instance.vertices]
+        list_points_vertices_Delaunay_triangulation = [] 
+        for simplex in hull_instance.simplices: #for each simplex (face; presumably a triangle) of the convex hull
+            convex_hull_triangular_facet_vertex_coordinates = self.original_point_array[simplex]
+            assert convex_hull_triangular_facet_vertex_coordinates.shape == (3,3), "Triangular facet of convex hull should be a triangle in 3D space specified by coordinates in a shape (3,3) numpy array."
+            list_points_vertices_Delaunay_triangulation.append(convex_hull_triangular_facet_vertex_coordinates)
+        array_points_vertices_Delaunay_triangulation = numpy.array(list_points_vertices_Delaunay_triangulation)
+        return array_points_vertices_Delaunay_triangulation
 
 
 
