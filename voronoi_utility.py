61bb71b58b87d86cfdfbdc71ba34209a945a6546
Tyler Reddy <tyler.je.reddy@gmail.com>
2014-11-03 20:57:43 +0000

    -voronoi_utility.py: 	-restructed Voronoi_polygons_spherical_surface() method to use a different approach--working based on a distance matrix between the Voronoi vertices and the generators 	-this seems to produce much more reasonable results based on inspection in ipynb

--- voronoi_utility.py
+++ voronoi_utility.py
@@ -112,99 +112,41 @@ class Voronoi_Sphere_Surface:
         array_Voronoi_vertices = produce_array_Voronoi_vertices_on_sphere_surface(facet_coordinate_array_Delaunay_triangulation,self.estimated_sphere_radius,self.sphere_centroid)
         assert facet_coordinate_array_Delaunay_triangulation.shape[0] == array_Voronoi_vertices.shape[0], "The number of Delaunay triangles should match the number of Voronoi vertices."
         #now, the tricky part--building up a useful Voronoi polygon data structure
-        facet_index_counter = 0
-        edge_dictionary = {} #store the coordinates of all Voronoi edges in the system, classified by the Voronoi vertex shared by all the edges
-        for Delaunay_facet_neighbour_index_array in self.hull_instance.neighbors:
-            voronoi_vertex_coordinate_corresponding_to_current_Delaunay_facet = array_Voronoi_vertices[facet_index_counter]
-            list_Voronoi_edges_including_current_facet = []
-            for index_of_neighbouring_facet in Delaunay_facet_neighbour_index_array:
-                if numpy.allclose(voronoi_vertex_coordinate_corresponding_to_current_Delaunay_facet, array_Voronoi_vertices[index_of_neighbouring_facet],atol=1e-7): 
-                    continue #avoid vertex-vertex 0-length edges
-                current_Voronoi_edge_array = numpy.array([voronoi_vertex_coordinate_corresponding_to_current_Delaunay_facet, array_Voronoi_vertices[index_of_neighbouring_facet]])
-                list_Voronoi_edges_including_current_facet.append(current_Voronoi_edge_array)
-            edge_dictionary[facet_index_counter] = numpy.array(list_Voronoi_edges_including_current_facet)
-            facet_index_counter += 1
-        #so, edge_dictionary should looking something like this for a given sample entry: {0: [ [Voronoi_edge_coord_1, Voronoi_edge_coord_2], [Voronoi_edge_coord_1, Voronoi_edge_coord_3], [Voronoi_edge_coord_1,Voronoi_edge_coord_4]], ...}
-        #also, I'm anticipating a lot of possible duplication of edges in edge_dictionary (i.e., edges shared with neighbours)
-        #furthermore, these would only be straight-line edges, rather than the desired great-circle edges, although the vertices are of course the same in both cases [so, it's really just a plotting issue I think]
-        #return edge_dictionary #temporary return for testing purposes
-        
-        #the Voronoi edges contained in edge_dictionary look reasonable by visual inspection, but we don't have the edges organized into polygons surrounding a single data point just yet
-        #edge_dictionary as constructed above actually seems to have a problem--different Voronoi_vertex_number values can have the same voronoi vertex in different edges, so I'll want to group these together:
-        grouped_edge_dictionary = {}
-        Voronoi_vertex_counter = 0
-        previous_voronoi_vertex_coordinate = 0
-        for index_number, array_Voronoi_edges_associated_with_this_index in edge_dictionary.iteritems():
-            voronoi_vertex_coordinate = array_Voronoi_edges_associated_with_this_index[0][0]
-            if numpy.allclose(voronoi_vertex_coordinate,previous_voronoi_vertex_coordinate,atol=1e-7): #already have this Voronoi vertex in the dictionary
-                grouped_edge_dictionary[Voronoi_vertex_counter] = numpy.concatenate((grouped_edge_dictionary[Voronoi_vertex_counter],array_Voronoi_edges_associated_with_this_index))
-            else: #new Voronoi vertex
-                Voronoi_vertex_counter +=1
-                grouped_edge_dictionary[Voronoi_vertex_counter] = array_Voronoi_edges_associated_with_this_index
-            previous_voronoi_vertex_coordinate = voronoi_vertex_coordinate
 
-        edge_dictionary = grouped_edge_dictionary
+        #new strategy--I already have the Voronoi vertices and the generators, so work based off a distance matrix between them
+        distance_matrix_Voronoi_vertices_to_generators = scipy.spatial.distance.cdist(array_Voronoi_vertices,self.original_point_array)
+        #now, each row of the above distance array corresponds to a single Voronoi vertex, which each column of that row representing the distance to the respective generator point
+        #if we iterate through each of the rows and determine the indices of the minimum distances, we obtain the indices of the generators for which that voronoi vertex is a polygon vertex 
+        generator_Voronoi_region_dictionary = {} #store the indices of the generators for which a given Voronoi vertex is also a polygon vertex
+        for Voronoi_point_index, Voronoi_point_distance_array in enumerate(distance_matrix_Voronoi_vertices_to_generators):
+            Voronoi_point_distance_array = numpy.around(Voronoi_point_distance_array,decimals=10)
+            indices_of_generators_for_which_this_Voronoi_point_is_a_polygon_vertex = numpy.where(Voronoi_point_distance_array == Voronoi_point_distance_array.min())[0]
+            assert indices_of_generators_for_which_this_Voronoi_point_is_a_polygon_vertex.size >= 3, "By definition, a Voronoi vertex must be equidistant to at least 3 generators."
+            generator_Voronoi_region_dictionary[Voronoi_point_index] = indices_of_generators_for_which_this_Voronoi_point_is_a_polygon_vertex #so dictionary looks like 0: array(12,17,27), ...
 
-        #I want to calculate the midpoints of all Voronoi edges to simplify downstream data structuring (the vertices can be equidistant to multiple generators and are more confusing to work with here I think)
-        edge_midpoint_dictionary = {} 
-        for Voronoi_vertex_number, array_Voronoi_edges_associated_with_this_vertex in edge_dictionary.iteritems():
-            #print '---------'
-            #print 'Voronoi_vertex_number:', Voronoi_vertex_number
-            #print 'array_Voronoi_edges_associated_with_this_vertex.shape:', array_Voronoi_edges_associated_with_this_vertex.shape
-            #print 'array_Voronoi_edges_associated_with_this_vertex:', array_Voronoi_edges_associated_with_this_vertex
-            #print '---------'
-            array_edge_midpoints_associated_with_this_vertex = numpy.average(array_Voronoi_edges_associated_with_this_vertex,axis=1)
-            edge_midpoint_dictionary[Voronoi_vertex_number] = array_edge_midpoints_associated_with_this_vertex
-        
-        #find and store the indices of the generator (original data) points that are closest to each Voronoi edge midpoint (some will have multiple closest points)
-        dictionary_closest_generator_indices_per_edge_midpoint = {}
-        for Voronoi_vertex_number, array_edge_midpoints_associated_with_this_vertex in edge_midpoint_dictionary.iteritems():
-            print '----------'
-            print 'Voronoi_vertex_number:', Voronoi_vertex_number
-            #print 'array_edge_midpoints_associated_with_this_vertex:', array_edge_midpoints_associated_with_this_vertex 
-            distance_matrix_Voronoi_edge_midpoints_to_original_data_points = scipy.spatial.distance.cdist(array_edge_midpoints_associated_with_this_vertex,self.original_point_array)
-            array_indices_for_generators_closest_to_edge_midpoints = numpy.argmin(distance_matrix_Voronoi_edge_midpoints_to_original_data_points,axis=1) #each row (midpoint) should have a set of indices for the closest original data points
-            print 'indices of generators closest to edge midpoints calculated from current vertex:', array_indices_for_generators_closest_to_edge_midpoints
-            print 'number of edge midpoints calculated using current Voronoi vertex that are closest to a given generator point:', array_indices_for_generators_closest_to_edge_midpoints.size
-            #print '----------'
-            dictionary_closest_generator_indices_per_edge_midpoint[Voronoi_vertex_number] = array_indices_for_generators_closest_to_edge_midpoints
-        #a representative element of the above dictionary looks like this: {0: array([104,114]),...}
+        #now, go through the above dictionary and collect the Voronoi point indices forming the polygon for each generator index
+        dictionary_Voronoi_point_indices_for_each_generator = {}
+        for Voronoi_point_index, indices_of_generators_for_which_this_Voronoi_point_is_a_polygon_vertex in generator_Voronoi_region_dictionary.iteritems():
+            for generator_index in indices_of_generators_for_which_this_Voronoi_point_is_a_polygon_vertex:
+                if generator_index in dictionary_Voronoi_point_indices_for_each_generator:
+                    list_Voronoi_indices = dictionary_Voronoi_point_indices_for_each_generator[generator_index] 
+                    list_Voronoi_indices.append(Voronoi_point_index)
+                    dictionary_Voronoi_point_indices_for_each_generator[generator_index] = list_Voronoi_indices
+                else: #initialize the list of Voronoi indices for that generator key
+                    dictionary_Voronoi_point_indices_for_each_generator[generator_index] = [Voronoi_point_index]
+        #so this dictionary should have format: {generator_index: [list_of_Voronoi_indices_forming_polygon_vertices]}
 
-        #now I want to reorganize the data structure such that I capture the Voronoi vertices that surround each generator (original data) point
-        #for any given generator, this should include all Voronoi edge midpoints that include that generator amongst their 'closest'
-        dictionary_generator_Voronoi_polygons = {}
-        for generator_index, generator_coordinate in enumerate(self.original_point_array):
-            #print 'generator_index:', generator_index
-            list_voronoi_vertices_current_generator = []
-            for Voronoi_vertex_number, array_indices_for_generators_closest_to_edge_midpoints in dictionary_closest_generator_indices_per_edge_midpoint.iteritems():
-                if generator_index in array_indices_for_generators_closest_to_edge_midpoints:
-                    indices_of_edges_bounding_current_generator = numpy.where(array_indices_for_generators_closest_to_edge_midpoints == generator_index)
-                    #print 'indices_of_edges_bounding_current_generator:', indices_of_edges_bounding_current_generator
-                    vertices_of_edges_bounding_current_generator = edge_dictionary[Voronoi_vertex_number][indices_of_edges_bounding_current_generator]
-                    for vertex in vertices_of_edges_bounding_current_generator:
-                        list_voronoi_vertices_current_generator.extend(vertex)
-            #try using pandas to remove duplicate rows (vertices) before storing in dictionary
-            array_Voronoi_vertices = numpy.around(numpy.array(list_voronoi_vertices_current_generator),decimals=8) #rounding decimals as pre-processing for duplicate removal
-            df = pandas.DataFrame(array_Voronoi_vertices)
-            #print 'df shape before:', df.shape
-            df.drop_duplicates(inplace=True)
-            #print 'df shape after deduplication:', df.shape
-            array_Voronoi_vertices = df.values #convert back to numpy array after dropping duplicates
-
-            #now, I want to sort the polygon vertices in a consistent, non-intersecting fashion
-            if array_Voronoi_vertices.shape[0] > 3:
-                polygon_hull_object = scipy.spatial.ConvexHull(array_Voronoi_vertices[...,:2]) #trying to project to 2D for edge ordering, and then restore to 3D after
+        #now, I want to sort the polygon vertices in a consistent, non-intersecting fashion
+        dictionary_sorted_Voronoi_point_coordinates_for_each_generator = {}
+        for generator_index, list_unsorted_Voronoi_region_vertices in dictionary_Voronoi_point_indices_for_each_generator.iteritems():
+            current_array_Voronoi_vertices = array_Voronoi_vertices[list_unsorted_Voronoi_region_vertices]
+            if current_array_Voronoi_vertices.shape[0] > 3:
+                polygon_hull_object = scipy.spatial.ConvexHull(current_array_Voronoi_vertices[...,:2]) #trying to project to 2D for edge ordering, and then restore to 3D after
                 point_indices_ordered_vertex_array = polygon_hull_object.vertices
-                array_Voronoi_vertices = array_Voronoi_vertices[point_indices_ordered_vertex_array]
+                current_array_Voronoi_vertices = current_array_Voronoi_vertices[point_indices_ordered_vertex_array]
+            dictionary_sorted_Voronoi_point_coordinates_for_each_generator[generator_index] = current_array_Voronoi_vertices
 
-            dictionary_generator_Voronoi_polygons[generator_index] = {'generator_coordinate':generator_coordinate,'voronoi_polygon_vertices':array_Voronoi_vertices}
-
-
-        return (edge_dictionary,edge_midpoint_dictionary,dictionary_closest_generator_indices_per_edge_midpoint,dictionary_generator_Voronoi_polygons) #temporary test return
-
-
-
-
+        return (generator_Voronoi_region_dictionary, dictionary_sorted_Voronoi_point_coordinates_for_each_generator)
 
         
 
