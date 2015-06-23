856d0e3301b4cbd67ecb887e7cb7d71b24cfa4d3
Edd Edmondson <eddedmondson@gmail.com>
2015-06-23 14:14:04 +0100

    Further fixes to spherical geometry, corrected Haversine distance, and new polygon area code implemented

--- voronoi_utility.py
+++ voronoi_utility.py
@@ -79,20 +79,19 @@ def calculate_haversine_distance_between_spherical_points(cartesian_array_1,cart
     lambda_2 = spherical_array_2[1]
     phi_1 = spherical_array_1[2]
     phi_2 = spherical_array_2[2]
-    spherical_distance = 2.0 * sphere_radius * math.asin(math.sqrt( ((1 - math.cos(phi_2-phi_1))/2.) + math.cos(phi_1) * math.cos(phi_2) * ( (1 - math.cos(lambda_2-lambda_1))/2.)  ))
+    #we rewrite the standard Haversine slightly as long/lat is not the same as spherical coordinates - phi differs by pi/4
+    spherical_distance = 2.0 * sphere_radius * math.asin(math.sqrt( ((1 - math.cos(phi_2-phi_1))/2.) + math.sin(phi_1) * math.sin(phi_2) * ( (1 - math.cos(lambda_2-lambda_1))/2.)  ))
     return spherical_distance
     
 def generate_random_array_spherical_generators(num_generators,sphere_radius,prng_object):
-    '''Generate and return an array of shape (num_generators,3) random points on a sphere of given radius.
-    Based on: Muller, M. E. "A Note on a Method for Generating Points Uniformly on N-Dimensional Spheres." Comm. Assoc. Comput. Mach. 2, 19-20, Apr. 1959.
-    And see: http://mathworld.wolfram.com/SpherePointPicking.html
-    prng_object is a pseudo random number generator object from numpy used for pinning down the random seed (so that pathological random states can be avoided as needed).
-    see: http://stackoverflow.com/a/5837352/2942522'''
-    #generate Gaussian random variables:
-    array_random_Gaussian_data = prng_object.normal(loc=0.0,scale=1.0,size=(num_generators,3)) 
-    #I'm not sure if this is the proper approach, but try projecting to the sphere_radius (may want to check above literature more closely):
-    spherical_polar_data = convert_cartesian_array_to_spherical_array(array_random_Gaussian_data)
-    spherical_polar_data[...,0] = sphere_radius
+    '''Recoded using standard uniform selector over theta and acos phi, http://mathworld.wolfram.com/SpherePointPicking.html
+    Same as in iPython notebook version'''
+    u = prng_object.uniform(low=0,high=1,size=num_generators)
+    v = prng_object.uniform(low=0,high=1,size=num_generators)
+    theta_array = 2 * math.pi * u
+    phi_array = numpy.arccos((2*v - 1.0))
+    r_array = sphere_radius * numpy.ones((num_generators,))
+    spherical_polar_data = numpy.column_stack((r_array,theta_array,phi_array))
     cartesian_random_points = convert_spherical_array_to_cartesian_array(spherical_polar_data)
     #filter out any duplicate generators:
     df_random_points = pandas.DataFrame(cartesian_random_points)
@@ -164,16 +163,28 @@ def calculate_surface_area_of_planar_polygon_in_3D_space(array_ordered_Voronoi_p
     return planar_polygon_surface_area
 
 def calculate_surface_area_of_a_spherical_Voronoi_polygon(array_ordered_Voronoi_polygon_vertices,sphere_radius):
-    '''Calculate the surface area of a polygon on the surface of a sphere. Based on equation provided here: http://mathworld.wolfram.com/SphericalPolygon.html'''
-    spherical_array_Voronoi_polygon_vertices_before_filtering = convert_cartesian_array_to_spherical_array(array_ordered_Voronoi_polygon_vertices)
-    array_ordered_Voronoi_polygon_vertices = filter_polygon_vertex_coordinates_for_extreme_proximity(array_ordered_Voronoi_polygon_vertices,sphere_radius) #filter vertices for extreme proximity
-    #theta = calculate_and_sum_up_inner_sphere_surface_angles_Voronoi_polygon(array_ordered_Voronoi_polygon_vertices,sphere_radius) #suppressing this calculation while I test the pure-planar SA calculation approach
+    '''Calculate the surface area of a polygon on the surface of a sphere. Based on equation provided here: http://mathworld.wolfram.com/LHuiliersTheorem.html'''
+    '''Decompose into triangles, calculate excess for each'''
     n = array_ordered_Voronoi_polygon_vertices.shape[0]
-    #surface_area_Voronoi_polygon_on_sphere_surface = (theta - ((n - 2) * math.pi)) * (sphere_radius ** 2) #commenting the spherical SA code to test a pure planar algorithm approach
-    surface_area_Voronoi_polygon_on_sphere_surface = calculate_surface_area_of_planar_polygon_in_3D_space(array_ordered_Voronoi_polygon_vertices) #just use planar area as estimate, which may be quite appropriate with many of the failure /edge cases in any case
-    if surface_area_Voronoi_polygon_on_sphere_surface <= 0: #the spherical polygon surface area could not be calculated properly (in the limit of large numbers of vertices and when vertices that are nearly co-planar are present this seems to be a problem)
-        surface_area_Voronoi_polygon_on_sphere_surface = calculate_surface_area_of_planar_polygon_in_3D_space(array_ordered_Voronoi_polygon_vertices) #just use planar area as estimate, which may be quite appropriate with many of the failure /edge cases in any case
-    return surface_area_Voronoi_polygon_on_sphere_surface
+    
+    #point we start from
+    root_point = array_ordered_Voronoi_polygon_vertices[0]
+    totalexcess = 0
+
+    #loop from 1 to n-2, with point 2 to n-1 as other vertex of triangle
+    # this could definitely be written more nicely
+    b_point = array_ordered_Voronoi_polygon_vertices[1]
+    root_b_dist = calculate_haversine_distance_between_spherical_points(root_point, b_point, 1.0)
+    for i in 1+numpy.arange(n-2):
+	    a_point = b_point
+	    b_point = array_ordered_Voronoi_polygon_vertices[i+1]
+	    root_a_dist = root_b_dist
+	    root_b_dist = calculate_haversine_distance_between_spherical_points(root_point, b_point, 1.0)
+	    a_b_dist = calculate_haversine_distance_between_spherical_points(a_point, b_point, 1.0)
+	    s = (root_a_dist + root_b_dist + a_b_dist) / 2
+	    totalexcess += 4 * math.atan(math.sqrt( math.tan(0.5 * s) * math.tan(0.5 * (s-root_a_dist)) * math.tan(0.5 * (s-root_b_dist)) * math.tan(0.5 * (s-a_b_dist))))
+	
+    return totalexcess
 
 def calculate_and_sum_up_inner_sphere_surface_angles_Voronoi_polygon(array_ordered_Voronoi_polygon_vertices,sphere_radius):
     '''Takes an array of ordered Voronoi polygon vertices (for a single generator) and calculates the sum of the inner angles on the sphere surface. The resulting value is theta in the equation provided here: http://mathworld.wolfram.com/SphericalPolygon.html '''
@@ -247,8 +258,8 @@ def convert_cartesian_array_to_spherical_array(coord_array,angle_measure='radian
     spherical_coord_array = numpy.zeros(coord_array.shape)
     xy = coord_array[...,0]**2 + coord_array[...,1]**2
     spherical_coord_array[...,0] = numpy.sqrt(xy + coord_array[...,2]**2)
-    spherical_coord_array[...,1] = numpy.arctan2(numpy.sqrt(xy), coord_array[...,2])
-    spherical_coord_array[...,2] = numpy.arctan2(coord_array[...,1],coord_array[...,0])
+    spherical_coord_array[...,1] = numpy.arctan2(coord_array[...,1], coord_array[...,0])
+    spherical_coord_array[...,2] = numpy.arccos(coord_array[...,2],spherical_coord_array[...,0])
     if angle_measure == 'degrees':
         spherical_coord_array[...,1] = numpy.degrees(spherical_coord_array[...,1])
         spherical_coord_array[...,2] = numpy.degrees(spherical_coord_array[...,2])
@@ -267,7 +278,7 @@ def convert_spherical_array_to_cartesian_array(spherical_coord_array,angle_measu
     cartesian_coord_array[...,1] = spherical_coord_array[...,0] * numpy.sin(spherical_coord_array[...,1]) * numpy.sin(spherical_coord_array[...,2])
     cartesian_coord_array[...,2] = spherical_coord_array[...,0] * numpy.cos(spherical_coord_array[...,2])
     return cartesian_coord_array
-
+    
 def produce_triangle_vertex_coordinate_array_Delaunay_sphere(hull_instance):
     '''Return shape (N,3,3) numpy array of the Delaunay triangle vertex coordinates on the surface of the sphere.'''
     list_points_vertices_Delaunay_triangulation = [] 
